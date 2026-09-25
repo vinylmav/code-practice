c68525163886db4a8fcfc49658e69d4077009a92
SSudhirAvinesh <175300372+ssudhiravinesh@users.noreply.github.com>
2026-09-25 22:35:52 +0530

    Stockspanner.py, subarray_min.py updated 2026-09-25 22:35

--- 2d-stack-and-monotonic-stack/Stockspanner.py
+++ 2d-stack-and-monotonic-stack/Stockspanner.py
@@ -1,22 +1,17 @@
 class StockSpanner:
 
     def __init__(self):
-        self.stack = [(100, 1)]
+        self.stack = []
 
     def next(self, price):
-        n = len(self.stack)
-        for i in range(n):
-            print("i", i)
-            span = 1
-            print(self.stack)
-            while self.stack and self.stack[-1][0] < price:
-                span += self.stack.pop()[-1]
-            self.stack.append((price, span))
-            print("*" * 20)
+        span = 1
+        while self.stack and self.stack[-1][0] <= price:
+            span += self.stack.pop()[-1]
+        self.stack.append((price, span))
         return self.stack[-1][-1]
 
 spanner = StockSpanner()
-# print(spanner.next(100))  # → 1
+print(spanner.next(100))  # → 1
 print(spanner.next(80))   # → 1
 print(spanner.next(60))   # → 1
 print(spanner.next(70))   # → 2
